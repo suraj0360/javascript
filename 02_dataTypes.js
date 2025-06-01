5cf5115bc34e3ccbb6c152e6b5e70c4b0a932115
Suraj <surajyadav0360@gmail.com>
2025-06-01 19:26:28 +0530

    datatypes

---
+++ 02_dataTypes.js
@@ -0,0 +1,24 @@
+ "use strict";  //treat all js code as new version
+
+//  alert(3+3) //we are using nodeJs, not browser
+
+console.log(3+
+    3)         //code should be readable
+
+    let name="suraj"
+    let age=18
+    let isLoggedIn=false
+
+
+//number = 2 to pwr 53
+//string = ""  
+//boolean = tyrue/false
+//bigint = use for bidg number
+//null = standalone value 
+//undefined = value not define
+//symbol = use for unique
+//object =
+
+console.log(typeof null);         //object
+console.log(typeof undefined);     //undefined, undefined khud me hi ek type hai
+
