8253930d2def175c649e7e52e91bd2b0883d9658
Suraj <surajyadav0360@gmail.com>
2025-06-01 18:07:49 +0530

    vaiable

---
+++ 01_variable.js
@@ -0,0 +1,12 @@
+const accountId=144553
+let accountEmail="suraj@google.com"
+var accountPassword="123456"
+accountCity="surat"
+let accountState;
+
+// accountId=2
+ console.log(accountId)
+accountPassword="5678"
+accountEmail="surajyadav@google.com"
+accountCity="Bengaluru"
+console.table([accountId,accountPassword,accountEmail,accountCity,accountState])
