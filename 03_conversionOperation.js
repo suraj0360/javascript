9825108fc0f9f225a67c6c6e6913949e187ec6f8
Suraj <surajyadav0360@gmail.com>
2025-06-07 11:12:06 +0530

    conversion of datatype

---
+++ 03_conversionOperation.js
@@ -0,0 +1,29 @@
+let score=true
+
+//we can check type of any variable in two ways
+console.log(typeof score);
+//    //OR
+console.log(typeof(score));   
+
+let ValueInNumber=Number(score)
+ console.log(typeof(ValueInNumber));
+console.log(ValueInNumber);
+
+//"33"= 33
+//"33abc"= NaN ,but its type is a number
+//true=1; false=0
+
+//boolean
+
+let isLoggedIn="suraj"
+let booleanIsLoggedIn=Boolean(isLoggedIn)
+ console.log(booleanIsLoggedIn);
+
+//1=true;  0=false
+//""= false
+//"suraj"=true
+
+let someNumber=33
+let stringNumber=String(someNumber)
+console.log(stringNumber);
+console.log(typeof(stringNumber))
