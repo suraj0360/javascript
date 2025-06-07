c52b6921ce3b3095233af1702236c14cad200952
Suraj <surajyadav0360@gmail.com>
2025-06-07 12:09:40 +0530

    conversionOperation

--- 03_conversionOperation.js
+++ 03_conversionOperation.js
@@ -1,23 +1,24 @@
 let score=true
 
+
 //we can check type of any variable in two ways
-console.log(typeof score);
-//    //OR
-console.log(typeof(score));   
+  console.log(typeof score);
+// // //    //OR
+  console.log(typeof(score));   
 
 let ValueInNumber=Number(score)
- console.log(typeof(ValueInNumber));
+console.log(typeof(ValueInNumber));
 console.log(ValueInNumber);
 
-//"33"= 33
-//"33abc"= NaN ,but its type is a number
-//true=1; false=0
+// //"33"= 33
+// //"33abc"= NaN ,but its type is a number
+// //true=1; false=0
 
-//boolean
+// //boolean
 
-let isLoggedIn="suraj"
-let booleanIsLoggedIn=Boolean(isLoggedIn)
- console.log(booleanIsLoggedIn);
+ let isLoggedIn="suraj"
+ let booleanIsLoggedIn=Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
 
 //1=true;  0=false
 //""= false
@@ -26,4 +27,35 @@ let booleanIsLoggedIn=Boolean(isLoggedIn)
 let someNumber=33
 let stringNumber=String(someNumber)
 console.log(stringNumber);
-console.log(typeof(stringNumber))
+console.log(typeof(stringNumber));
+
+
+// *****************operation*******************
+
+let value=3
+let negvalue=-value
+console.log(negvalue);
+
+
+
+console.log(2+3);
+console.log(2-3);
+console.log(2*3);
+console.log(2**3);
+console.log(2/3);
+console.log(2%3);
+
+let str1="hello"
+let str2=" suraj"
+let str3=str1+str2
+console.log(str3);
+console.log("1"+2);
+console.log(1+"2");
+console.log("1"+2+2);
+console.log(1+2+"2");
+console.log((3+5)*4%2);
+console.log(true)
+
+let gamecounter=100
+gamecounter++
+console.log(gamecounter);
