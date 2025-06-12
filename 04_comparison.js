eed3382f2eb5e4e6b26284485fc82fa7a4577bb4
Suraj <surajyadav0360@gmail.com>
2025-06-12 23:29:55 +0530

    datatype-summary

--- 04_comparison.js
+++ 04_comparison.js
@@ -7,6 +7,8 @@ console.log(2>=1);
 console.log("2" + 1);
 console.log("2"+"2"+1);
 console.log(2+1+"2");
+console.log("0"+"2"+1)
+console.log("02"+3)
 
 console.log(null > 0);
 console.log(null < 0);
---
+++ datatype-summary.js
@@ -0,0 +1,18 @@
+// # primitive 
+ 
+//7 types: String, Number, BigInt, Boolean, symbol, null, undefined
+
+const score=100
+const scvorevalue=100.3
+
+const isLoggedIn=true
+let outsidetemp=null
+let city;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+console.log(id===anotherId);
+
+//reference(non primitive)
+
+//Array, object, funtion
