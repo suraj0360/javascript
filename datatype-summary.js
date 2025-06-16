5a0a1e93e55463fdfbeecfd8652d1c31c667eed9
Suraj <surajyadav0360@gmail.com>
2025-06-16 17:54:34 +0530

    datatype-summary

--- datatype-summary.js
+++ datatype-summary.js
@@ -13,6 +13,18 @@ const id = Symbol('123')
 const anotherId = Symbol('123')
 console.log(id===anotherId);
 
+const bigNumber=832050024239n
+
 //reference(non primitive)
 
 //Array, object, funtion
+
+const heros=["shaktiman","naagraj","doga"];
+let myObj={
+    name:suraj,
+    age:20,
+}
+
+const myFunction=function(){
+    console.log("hello world");
+}
