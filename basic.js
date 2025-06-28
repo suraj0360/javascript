35a3b01bd2d5d613799fd4b4a7c80451fadea51c
Suraj <surajyadav0360@gmail.com>
2025-06-28 21:05:17 +0530

    Num and Math(method)

---
+++ basic.js
@@ -0,0 +1,92 @@
+                              //variable 
+console.log("suraj");
+const accountId=123459
+let accountEmail="suraj@google.com"
+var accountPassword="12345"
+accountCity="surat"
+
+
+
+console.table([accountId,accountEmail,accountPassword,accountCity]);
+console.log(typeof(accountId));
+console.log(typeof(accountEmail));
+console.log(typeof(accountPassword));
+console.log(typeof(accountCity));
+
+                               //datatypes
+
+// different types
+
+//string
+let Name="suraj"
+console.log(Name);
+
+//number
+const Id=24345
+console.log(Id);
+
+//boolean
+let isLoggedIn=true
+console.log(isLoggedIn);
+
+//symbol           
+let HomeId=123               //it is uniqe
+let anotherHomeId="123"
+console.log(HomeId===anotherHomeId);
+
+
+//object
+// let myObj={
+//     Name:suraj,
+//     age:18,
+// }
+
+
+//bigint
+let num=1283679n
+console.log(num);
+
+//null
+let temp=null;
+console.log(typeof(temp));
+console.log(temp);
+
+
+//undefined
+let temp1;
+console.log(typeof(temp1));
+console.log(temp);
+
+
+//conversionOperation
+
+console.log(2+2)
+console.log(2-2)
+console.log(2*2)
+console.log(2**2)
+console.log(2/2)
+console.log(2%2)
+
+console.log("1"+2+"2")
+console.log(1+2+4+"2")
+console.log("3"+2+4+5+8)
+
+console.log(null==0)
+console.log(null<0)
+console.log(null>0)
+console.log(null<=0)
+console.log(null>=0)
+
+console.log(undefined=0)
+console.log(undefined==0)
+console.log(undefined<0)
+console.log(undefined>0)
+console.log(undefined<=0)
+console.log(undefined>=0)
+
+
+console.log(typeof(undefined));
+console.log(typeof(null));
+console.log(typeof(object));
+console.log(typeof(Symbol));
+
--- datatype-summary.js
+++ datatype-summary.js
@@ -2,29 +2,55 @@
  
 //7 types: String, Number, BigInt, Boolean, symbol, null, undefined
 
-const score=100
-const scvorevalue=100.3
+// const score=100
+// const scvorevalue=100.3
 
-const isLoggedIn=true
-let outsidetemp=null
-let city;
+// const isLoggedIn=true
+// let outsidetemp=null
+// let city;
 
-const id = Symbol('123')
-const anotherId = Symbol('123')
-console.log(id===anotherId);
+// const id = Symbol('123')
+// const anotherId = Symbol('123')
+// console.log(id===anotherId);
 
-const bigNumber=832050024239n
+// const bigNumber=832050024239n
 
-//reference(non primitive)
+// //reference(non primitive)
 
-//Array, object, funtion
+// //Array, object, funtion
 
-const heros=["shaktiman","naagraj","doga"];
-let myObj={
-    name:suraj,
-    age:20,
+// const heros=["shaktiman","naagraj","doga"];
+// let myObj={
+//     name:suraj,
+//     age:20,
+// }
+
+// const myFunction=function(){
+//     console.log("hello world");
+// }
+
+
+/////+++++++++++++++++++++++++++++++++++++++++++++++
+
+//stack (jitne bhi primitive datatypes hai)
+//heap (jitne bhi non primitive datatypes hai)
+
+// let Myyoutubename="surajyadav@google.com"
+
+// let anothername=Myyoutubename
+// console.log(anothername);
+// anothername="vfvfewipdshjbfckilko;8"
+// console.log(anothername);
+// console.log(Myyoutubename);
+
+let userone={
+    email:"user@google.com",
+    upi:"user@ybl"
 }
 
-const myFunction=function(){
-    console.log("hello world");
-}
+let usertwo=userone
+ console.log(usertwo);
+
+usertwo.email="userfglij@google.com"
+console.log(usertwo);
+console.log(userone);
